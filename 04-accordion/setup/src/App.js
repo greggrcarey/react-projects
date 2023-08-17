3af082a49c1e284ecc6ed6bb1902a69f5a9a2973
Gregg Carey <gregg.carey@outlook.com>
2023-08-17 14:11:09 -0400

    questions with dropdown

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,24 @@
 import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
-function App() {
-  return <h2>accordion project setup</h2>;
+import Question from './Question';
+function App()
+{
+  const [questions, setQuestions] = useState(data);
+  return (<main>
+    <div className='container'>
+      <h3>
+        questions and answers about login
+      </h3>
+      <section className='info'>
+        {questions.map((question) =>
+        {
+          return <SingleQuestion key={question.id} {...question} />;
+        })}
+      </section>
+    </div>
+  </main>);
+
 }
 
 export default App;
