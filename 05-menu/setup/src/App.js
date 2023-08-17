172a811fe236389d14772909b8923c790841d540
Gregg Carey <gregg.carey@outlook.com>
2023-08-17 15:47:36 -0400

    Set object for categories, filter based on categor

--- 05-menu/setup/src/App.js
+++ 05-menu/setup/src/App.js
@@ -3,8 +3,34 @@ import Menu from './Menu';
 import Categories from './Categories';
 import items from './data';
 
-function App() {
-  return <h2>menu project setup</h2>;
+const allCategories = ['all', ...new Set(items.map((item) => item.category))]
+
+function App()
+{
+  const [menu, setMenu] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) =>
+  {
+    if (category === 'all')
+    {
+      setMenu(items)
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenu(newItems);
+  }
+
+  return <main className='container'>
+    <section className="menu">
+      <div className="title">
+        <h2>our menu</h2>
+      </div>
+      <div className="underline"></div>
+      <Categories categories={categories} filterItems={filterItems} />
+      <Menu items={menu} />
+    </section>
+  </main>
 }
 
 export default App;
